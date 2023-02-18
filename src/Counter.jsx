2c2523448e15ed0f5556e1faba43d0e571cca948
ZabdiBen <montaeze@gmail.com>
2023-02-18 22:34:15 +0200

    added talwind

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,19 @@
+import PropTypes from 'prop-types';
+
 export const Counter = ({value}) => {
   return (
     <>
-      <section>
-         <div>
+      <section class="text-white p-1">
+         <div class="flex flex-col">
           <h1>Counter App</h1>
           <span>{value}</span>
-          <button>Add 1+</button>
+          <button class="bg-white text-black w-1/3">Add 1+</button>
          </div>
       </section>
     </>
     )
+}
+
+Counter.propTypes = {
+  title: PropTypes.number
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {FirstApp} from "./FirstApp";
+//import {FirstApp} from "./FirstApp";
+import {Counter} from './Counter'
 import "./styles.css"
 
 ReactDOM.createRoot( document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title="hola" />
+    <Counter value={10} />
   </React.StrictMode>
   )
