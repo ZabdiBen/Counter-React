d43a12c135f1edaa0b0622cfe00c0a88a75bec4a
ZabdiBen <montaeze@gmail.com>
2023-02-22 07:51:37 +0200

    añadido un hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,23 @@
+/*
+use nos dice que es un hook.
+*/
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
 export const Counter = ({value}) => {
+  
+  const [ counter, setCounter] = useState(value);
+  
+  const add = () => {
+  setCounter( counter + 1 )
+}
   return (
     <>
-      <section class="text-white p-1">
+      <section class="text-white p-3">
          <div class="flex flex-col">
-          <h1>Counter App</h1>
-          <span>{value}</span>
-          <button class="bg-white text-black w-1/3">Add 1+</button>
+          <h1 class="pb-4">Counter App</h1>
+          <span class="pb-4">{counter}</span>
+          <button onClick={add} class="bg-white text-black w-1/3">Add 1+</button>
          </div>
       </section>
     </>
