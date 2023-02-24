5c0e043d9e853257dea5e64f0b9eebf4fc0cb860
ZabdiBen <montaeze@gmail.com>
2023-02-24 13:54:00 +0200

    finished my frist simple app in react

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,26 +4,32 @@ use nos dice que es un hook.
 import {useState} from 'react';
 import PropTypes from 'prop-types';
 
-export const Counter = ({value}) => {
+export const Counter= ({value}) => {
   
-  const [ counter, setCounter] = useState(value);
+const [ counter, setCounter]= useState(value);
   
-  const add = () => {
-  setCounter( counter + 1 )
-}
+const add= () => setCounter( counter + 1)
+const rest= () => setCounter(counter - 1)
+const reset=()=> setCounter(value)
   return (
     <>
       <section class="text-white p-3">
-         <div class="flex flex-col">
-          <h1 class="pb-4">Counter App</h1>
-          <span class="pb-4">{counter}</span>
-          <button onClick={add} class="bg-white text-black w-1/3">Add 1+</button>
+        <div class="flex flex-col">
+        <h1 class="pb-4">Counter App</h1>
+        <span class="pb-4">{counter}</span>
+        
+        <div class='flex justify-around'>
+        <button onClick={add} class="bg-white text-black w-1/3 ">Add 1+</button>
+        <button onClick={rest} class="bg-white text-black w-1/3 mx-1">- 1</button>
+        <button onClick={reset} class="bg-white text-black w-1/3">reset</button>
+        </div>
+        
          </div>
-      </section>
+       </section>
     </>
     )
 }
 
-Counter.propTypes = {
-  title: PropTypes.number
+Counter.propTypes= {
+title: PropTypes.number
 }
